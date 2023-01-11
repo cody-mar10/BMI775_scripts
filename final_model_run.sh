9654b6e591cc3ee4b637aeb95201b3490b6a82a6
Cody Martin <cody.martin@tamu.edu>
2023-01-11 10:39:34 -0600

    CHTC job executables and submit files

---
+++ final_model_run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while getopts g:l:L:a: option; do
+    case $option in
+	g) gnn=$OPTARG;;
+	l) lin1=$OPTARG;;
+	L) lin2=$OPTARG;;
+	a) attention=$OPTARG;;
+    esac
+done
+
+cat <<EOF
+
+Convolution hidden dimension: $gnn
+MLP hidden dimensions: ($lin1, $lin2)
+Attention heads: $attention
+
+EOF
+
+set -e
+ENVNAME="pyg-gpu"
+ENVDIR=$ENVNAME
+
+cp /staging/groups/burton_group/tmpcody/pyg-gpu.tar.gz .
+export PATH
+mkdir $ENVDIR
+tar -xzf $ENVNAME.tar.gz -C $ENVDIR
+. $ENVDIR/bin/activate
+
+python3 main.py -i cherry_heterodataset.pt -e 100 -k 10 -gh $gnn -lh $lin1 $lin2 -d 0.25 -w 0.0 -a $attention -c GAT -s CHERRY_GAT.model.pt -n 3
+
+rm -rf $ENVDIR pyg-gpu.tar.gz
